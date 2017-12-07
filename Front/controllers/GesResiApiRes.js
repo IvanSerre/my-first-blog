7e37a056044a015e13d1e92a289170bbe86f8d87
Ivan Serre <ivan.serre@outlook.com>
2017-12-07 10:05:33 -0300

    Signed-off-by: Ivan Serre <ivan.serre@outlook.com>

--- Front/controllers/GesResiApiRes.js
+++ Front/controllers/GesResiApiRes.js
@@ -1,12 +1,22 @@
 // Encerrar nuestro javascript en paréntesis es buen hábito.
-(function() {
+(
+function()
+{
     var app = angular.module('ModuloResidentes', []); // Declaramos un modulo y el nombre de la aplicación "tienda"
-
     app.controller('Residentes', function($scope, $http){
-     debugger;
-     $http.get("http://127.0.0.1:8000/ListResidentes/?format=json").then(function(response){
-      debugger;
-       this.inforesidente = response.data;
-     });
+
+     Objresidentes  = this;
+     $http({
+           method:'get',
+           url: 'http://ivanserre.pythonanywhere.com/ListResidentes/',
+           params: {
+            format: 'json',
+            callback: 'JSON_CALLBACK'
+           }
+         }).then(function(res){
+           Objresidentes.inforesidente = res.data;
+         }, function(error){
+            alert(error.data);
+         });
     });
 })();
